b79f5609fff7e145b9a24e93f008f4c24ec66725
Anton Emin <anton.emin.work@gmail.com>
2022-08-26 19:42:05 +0000

    Small fix

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/
 echo "FILE=new_file" >> vars
-echo -e '#!/bin/bash\n . vars\n NAME=Anton_Emin\n mkdir ${NAME}\n touch ${NAME}/${FILE}\n ls \n ls ${NAME} ' >> ~/vars_script.sh
-chmod +x ~/vars_script.sh
-. ~/vars_script.sh
+echo -e '#!/bin/bash\n . vars\n NAME=Anton_Emin\n mkdir ${NAME}\n touch ${NAME}/${FILE}\n ls \n ls ${NAME} ' >> vars_script.sh
+chmod +x vars_script.sh
+. vars_script.sh
